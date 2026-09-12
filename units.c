f077bab0513c7e674de124b7679e8235fb9b0476
obed2-wavy <obedwere94@gmail.com>
2026-09-12 15:18:41 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,20 @@
+
+/*
+Author:Obed Onyango
+Reg Number:BCS-05-0075/2026
+Deascription:Units offered this semester program
+Date:12/09/2026
+*/
+#include <stdio.h>
+int main()
+{
+    //Display the academic units offered this semester
+    printf("Units Offered This Semester:\n");
+    printf("1Introduction to Programming\n");
+    printf("2Data structuers\n");
+    printf("3Computer Networks\n");
+    printf("4Database Systems\n");
+    printf("5Software Engineering\n");
+
+    return 0;
+}
